3d78873dd637708101d39795e823ab3bb8b9c6d8
Miao Zhao <miao_zhao@yahoo.com>
2022-07-10 22:10:51 +1000

    Implement `sendEmail` function

--- handler.ts
+++ handler.ts
@@ -1,19 +1,71 @@
-import { Handler } from 'aws-lambda';
+import { Handler } from "aws-lambda";
+import * as nodemailer from "nodemailer";
 
-export const sendEmail: Handler = (event: any) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+export const sendEmail: Handler = async (event: any) => {
+  const headers = {
+    "content-type": "application/json",
   };
 
-  return new Promise((resolve) => {
-    resolve(response)
-  })
-}
+  try {
+    console.log(event);
+    const { body } = event;
+    let requestBody = body;
+    if (typeof body !== "object") {
+      requestBody = JSON.parse(body);
+    }
+    const { to, subject, text, html } = requestBody;
+    if (!to || !subject || !text || !html) {
+      return {
+        statusCode: 400,
+        headers,
+        body: JSON.stringify(
+          {
+            message: `Function rejected due to incomplete request.`,
+            requestBody: requestBody,
+          },
+          null,
+          2
+        ),
+      };
+    }
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.sendgrid.net",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "apikey",
+        pass: "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",
+      },
+    });
+
+    transporter.verify(async (error, success) => {
+      if (error) {
+        console.error("Error when setting up sender: ", error);
+      }
+      if (success) {
+        console.log("Sender is verified and ready to send email.");
+        await transporter
+          .sendMail({
+            from: '"A-COMOSUS 🍍" <project.a.comosus@gmail.com>',
+            ...requestBody,
+          })
+          .then(() => {
+            console.log(`Email sent to ${to} successfully!`);
+          })
+          .catch(err => {
+            console.error(`Failed to send email with ${err}`);
+          });
+      }
+    });
+  } catch (err) {
+    console.log("err!", err);
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({
+        message: `Function errored when processing this request.${err}`,
+      }),
+    };
+  }
+};
