de8480475682b3b53785a3b830ae27d96cee6277
Norris Wu <42906455+NorrisWu0@users.noreply.github.com>
2022-07-23 10:41:48 +1000

    Add logging detail to getProjectInfo function

--- functions/getGitHubOrgInfo/index.ts
+++ functions/getGitHubOrgInfo/index.ts
@@ -13,12 +13,16 @@ export const handler: Handler = async () => {
   };
 
   try {
-    console.log("Receiving request to get Github organisation info...");
+    console.log("Receiving request to get project info from Github...");
 
+    console.log("Fetching organisation info...");
     const orgInfo = await getOrg();
+    console.log("Fetching team member info...");
     const members = await getMembers(await getOrgMember());
+    console.log("Fetching repo info...");
     const repos = await getRepoLanguages(await getRepos());
 
+    console.log("Github data processed and returning pproject info...");
     return {
       statusCode: 200,
       headers,
@@ -29,6 +33,12 @@ export const handler: Handler = async () => {
       }),
     };
   } catch (err) {
-    console.error(JSON.stringify(err, null, 2));
+    const message = `Exception found when collating github info... [${err.message}]`;
+    console.error(message);
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ message }),
+    };
   }
 };
