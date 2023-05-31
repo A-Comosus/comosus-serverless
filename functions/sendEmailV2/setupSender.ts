6cbfb1f84dc49ce6811f773a4e27b856eb3c0bdd
Cameron Huang <73293255+Cameron933@users.noreply.github.com>
2023-05-31 22:22:58 +1000

    update functions in yml file

--- functions/sendEmailV2/setupSender.ts
+++ functions/sendEmailV2/setupSender.ts
@@ -1,7 +1,7 @@
 import * as sgMail from "@sendgrid/mail";
 
 export function setupSender() {
-  sgMail.setApiKey(process.env.Sendgrid_API as string);
+  sgMail.setApiKey(process.env.SENDGRID_API as string);
 
   return sgMail;
 }
